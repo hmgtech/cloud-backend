bdb541e4369bedc0ef6f4d765290d0750255a701
Hitesh Gupta <59255201+hmgtech@users.noreply.github.com>
2024-04-08 14:15:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ username = os.getenv("DBUSERNAME")
 password = os.getenv("PASSWORD")
 host = os.getenv("HOST")
 database_name = os.getenv("DATABASE")
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_session_token = os.getenv("AWS_SESSION_TOKEN")
 
 print(database_name)
 
@@ -380,7 +383,13 @@ Best regards,
 
     # try:
         # Initialize boto3 client for AWS Lambda
-    session = boto3.Session()
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token
+    )
+
+    # Create a Lambda client using the session
     lambda_client = session.client('lambda', region_name='us-east-1')
 
     # Payload to be passed to the Lambda function
