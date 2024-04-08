1abb9cb66877112ffff669157f15c5c5b3c58720
Hitesh Gupta <59255201+hmgtech@users.noreply.github.com>
2024-04-08 16:59:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ Best regards,
 
     # Invoke the Lambda function
     response = lambda_client.invoke(
-        FunctionName='arn:aws:lambda:us-east-1:730335289956:function:agileTrackLambda2',
+        FunctionName='arn:aws:lambda:us-east-1:730335289956:function:agileTrackLambda',
         InvocationType='RequestResponse',  # Synchronous invocation
         Payload=json.dumps(payload)
     )
